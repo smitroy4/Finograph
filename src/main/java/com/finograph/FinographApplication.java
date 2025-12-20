4469a77d793f1983a8b2b0ef7472b0c4035934e8
Smit Roy <ttssmitherio@gmail.com>
2025-12-21 02:07:12 +0530

    Project Created

---
+++ src/main/java/com/finograph/FinographApplication.java
@@ -0,0 +1,13 @@
+package com.finograph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinographApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinographApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/finograph/FinographApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finograph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinographApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
